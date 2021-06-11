c4ff44c7e1d77c2cb24cbca7f4b4366d835b3464
Deniss Fjodorovs <3starblaze@gmail.com>
2021-06-11 20:39:10 +0300

    Initialize

---
+++ org-super-clock-report.el
@@ -0,0 +1,19 @@
+;;; org-super-clock-report.el --- Supercharged clock reports -*- lexical-binding: t; -*-
+;;
+;; Version: 0.0.1
+;; Keywords:
+;; Package-Requires: ((emacs 26.3) (cl-lib "0.5"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; org-super-clock-report provides a bunch of features to make clock reports
+;; more convenient.
+;;
+;;; Code:
+
+(require 'cl-lib)
+
+(provide 'org-super-clock-report)
+;;; org-super-clock-report.el ends here
