34435efe2be67963dd705a597a3e98b7e72ff83e
Deniss Fjodorovs <3starblaze@gmail.com>
2021-06-13 09:36:54 +0300

    Write MVP for table gen and test some functions
    Right now get-ast, parse-ast and regexp-headlines are confirmed to work.
    Rest of the utilities need to be tested because the table gen does not
    work at the moment.

--- org-super-clock-report.el
+++ org-super-clock-report.el
@@ -14,6 +14,59 @@
 ;;; Code:
 
 (require 'cl-lib)
+(require 'org-element)
+
+(defun org-super-clock-report--get-ast (buffer-or-name)
+  "Get AST of a buffer or its name BUFFER-OR-NAME."
+  (with-current-buffer buffer-or-name
+    (org-element-parse-buffer)))
+
+(defun org-super-clock-report--parse-ast (ast fun)
+  "Apply FUN for every element in AST."
+    (when ast
+      (apply fun (list ast))
+      (dolist (child (cddr ast))
+        (org-super-clock-report--parse-ast child fun))))
+
+(defun org-super-clock-report--regexp-headlines (ast regexp)
+  "Find headlines in AST that match REGEXP."
+  (let ((headline-asts))
+    (org-super-clock-report--parse-ast
+     ast
+     (lambda (this-ast)
+       (when (and (eq (cl-first this-ast) 'headline)
+                  (string-match-p regexp (plist-get (cl-second this-ast) :raw-value)))
+         (setf headline-asts (append headline-asts (list this-ast))))))
+    headline-asts))
+
+(defun org-super-clock-report--count-clock-duration (ast)
+  "For every clock in AST count minutes."
+  (let ((total-duration 0))
+    (org-super-clock-report--parse-ast
+     ast
+     (lambda (this-ast)
+       (let ((tmp-duration (and (eq (cl-first this-ast) 'headline)
+                                (plist-get (cl-second this-ast) :duration))))
+         (when tmp-duration
+           (cl-incf total-duration (org-duration-to-minutes tmp-duration))))))
+    total-duration))
+
+(defun org-super-clock-report-from-regexp (regexp)
+  "Create clock report from REGEXP."
+  (let* ((ast (org-super-clock-report--get-ast (current-buffer)))
+         (target-asts (org-super-clock-report--regexp-headlines ast regexp))
+         (headline-duration-plist))
+    (dolist (this-ast target-asts)
+      (plist-put
+       headline-duration-plist
+       (plist-get (cl-second this-ast) :raw-value)
+       (org-super-clock-report--count-clock-duration this-ast)))
+    (with-output-to-temp-buffer "*org-super-clock-report*"
+      (org-mode)
+      (print "| Headline | Duration |\n")
+      (cl-do ((this-plist headline-duration-plist (cddr this-plist)))
+          ((not this-plist) nil)
+        (print (concat "|" (cl-first this-plist) "|" (cl-second this-plist) "|\n"))))))
 
 (provide 'org-super-clock-report)
 ;;; org-super-clock-report.el ends here
--- test/test.el
+++ test/test.el
@@ -4,7 +4,36 @@
 ;;
 ;;; Code:
 
+(require 'org-super-clock-report (expand-file-name "../org-super-clock-report.el"))
 
+(defvar org-super-clock-report-test-org-file "test.org"
+  "Org mode file used for testing org-super-clock-report features.")
+
+(defvar org-super-clock-report-test-ast
+  (org-super-clock-report--get-ast org-super-clock-report-test-org-file)
+  "The AST representation of `org-super-clock-report-test-org-file'.")
+
+(ert-deftest org-super-clock-report-test-get-ast ()
+  ;; Full-file org-file AST start with org-data
+  (should (eq (cl-first org-super-clock-report-test-ast) 'org-data)))
+
+(ert-deftest org-super-clock-report-test-parse-ast ()
+  "Test parse-ast by checking if the file contains 7 headlines."
+  (let ((headline-counter 0))
+    (org-super-clock-report--parse-ast
+     org-super-clock-report-test-ast
+     (lambda (this-ast)
+       (when (eq (cl-first this-ast) 'headline)
+         (cl-incf headline-counter))))
+    (should (eq headline-counter 7))))
+
+(ert-deftest org-super-clock-report-test-regexp-headlines ()
+  (let ((asts (org-super-clock-report--regexp-headlines
+               org-super-clock-report-test-ast
+               "render")))
+    (should (eq (length asts) 1))
+    (should (equal (plist-get (cl-second (cl-first asts)) :raw-value)
+                   "Wait for render to finish"))))
 
 (provide 'test)
 ;;; test.el ends here
