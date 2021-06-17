83d58875902ed80e1edf352d1dcc2f4adb3646bf
Deniss Fjodorovs <3starblaze@gmail.com>
2021-06-17 13:55:23 +0300

    Create with-org-file-as-current-buffer test macro
    Initially it was made to fix the issue where if ERT is not run in the
    test directory, it doesn't open the proper org file, thus failing the
    tests but this commit does NOT fix it.

--- test/test.el
+++ test/test.el
@@ -13,6 +13,15 @@
   (org-super-clock-report--get-ast org-super-clock-report-test-org-file)
   "The AST representation of `org-super-clock-report-test-org-file'.")
 
+(defmacro org-super-clock-report-test--with-org-file-as-current-buffer
+    (&rest body)
+  "Use `org-super-clock-report-test-org-file' and feed BODY to `with-current-buffer'."
+  `(with-current-buffer
+       (find-file (expand-file-name
+                   org-super-clock-report-test-org-file
+                   (file-name-directory buffer-file-name)))
+     ,@body))
+
 (ert-deftest org-super-clock-report-test-get-ast ()
   ;; Full-file org-file AST start with org-data
   (should (eq (cl-first org-super-clock-report-test-ast) 'org-data)))
@@ -28,27 +37,27 @@
     (should (eq headline-counter 7))))
 
 (ert-deftest org-super-clock-report-test-regexp-filter ()
-  (with-current-buffer (find-file org-super-clock-report-test-org-file)
-    (should (equal (org-super-clock-report--query
-                    (org-super-clock-report--create-regexp-headline-filter
-                     "render"))
-                   '("Wait for render to finish" "3:17")))))
+  (org-super-clock-report-test--with-org-file-as-current-buffer
+   (should (equal (org-super-clock-report--query
+                   (org-super-clock-report--create-regexp-headline-filter
+                    "render"))
+                  '("Wait for render to finish" "3:17")))))
 
 (ert-deftest org-super-clock-report-test-headline-list-filter ()
-  (with-current-buffer (find-file org-super-clock-report-test-org-file)
-    (should (equal (org-super-clock-report--query
-                    (org-super-clock-report--create-list-headline-filter
-                     '("Make a puzzle")))
-                   '("Make a puzzle" "6:16")))))
+  (org-super-clock-report-test--with-org-file-as-current-buffer
+   (should (equal (org-super-clock-report--query
+                   (org-super-clock-report--create-list-headline-filter
+                    '("Make a puzzle")))
+                  '("Make a puzzle" "6:16")))))
 
 (ert-deftest org-super-clock-report-test-from-timestamp-clock-filter ()
-  (with-current-buffer (find-file org-super-clock-report-test-org-file)
-    (should (equal (org-super-clock-report--query
-                    (org-super-clock-report--create-regexp-headline-filter
-                     "busy")
-                    (org-super-clock-report--create-from-timestamp-clock-filter
-                     "[2021-05-10]"))
-                   '("Pretend to be busy" "2:29")))))
+  (org-super-clock-report-test--with-org-file-as-current-buffer
+   (should (equal (org-super-clock-report--query
+                   (org-super-clock-report--create-regexp-headline-filter
+                    "busy")
+                   (org-super-clock-report--create-from-timestamp-clock-filter
+                    "[2021-05-10]"))
+                  '("Pretend to be busy" "2:29")))))
 
 (provide 'test)
 ;;; test.el ends here
